eba0031ae12516cb897694b67a48402d9465416f
Gohar <guohan220323@gmail.com>
2026-06-04 01:27:37 +0400

    fin

--- sast_tool/rules/__init__.py
+++ sast_tool/rules/__init__.py
@@ -1,6 +1,3 @@
-# sast_tool/rules/__init__.py
 from . import sec_001_dangerous_calls
 from . import sec_002_dangerous_eval
-
-# If you have sec_999_test.py in that folder, uncomment the line below:
-# from . import sec_999_test
+from . import sec_999_test
